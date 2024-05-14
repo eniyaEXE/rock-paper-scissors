7a8ed3625cb91b336e1eab6f8390a94c762c0a9b
Eniya <161463527+eniyaEXE@users.noreply.github.com>
2024-05-14 07:48:27 +0000

    rock paper scissors v4
    - restructured most of the code for readability and efficiency
    - removed unnecessary sleep times (I added these for aesthetic reasons before but I don't like them anymore lol)
    - added colored text for readability
    - added a win percentage

--- ROCK PAPER SCISSORS.py
+++ ROCK PAPER SCISSORS.py
@@ -1,72 +1,70 @@
 # ROCK PAPER SCISSORS
 import random
-import time
 
-def clear():
-    for _ in range(0, 10):
-        print("\n")
-
-clear()
-print("welcome!")
-time.sleep(1)
-
-loopRunning = 1
 errorCount = 0
 wins = 0
 losses = 0
 draws = 0
+gamesPlayed = 0
 
-while loopRunning == 1:
+red = "\033[31m"        # ANSI color codes borrowed from graphing-calculator
+green = "\033[32m"
+blue = "\033[34m"
+yellow = "\033[33m"
+clear = "\033[0m"
+
+moveDict = {"rock": 3, 
+            "scissors": 2, 
+            "paper": 1,
+            "correction": -3
+            }
+    
+pcMoveDict = {3: "rock", 
+              2: "scissors", 
+              1: "paper", 
+              -3: "rock"
+              }
+
+print("welcome!")
+
+while True:
     pcMove = random.randint(1,3)                # randomly selects rock, paper, or scissors
-    playerMove = input("\nplease type rock, paper, or scissors (Q to quit): ")        # asks player for move
-    moveDict = {"rock": 3, "scissors": 2, "paper": 1}
-    pcMoveDict = {3: "rock", 2: "scissors", 1: "paper", -3: "rock"}
-
-    if playerMove == "Q":
-        clear()
-        loopRunning = 0
-        continue
-
-    if pcMove == 1 and moveDict.get(playerMove) == 3: # small workaround for paper beats rock
-        time.sleep(0.6)
-        print("\nthe computer chose paper...")
-        time.sleep(0.3)
-        print("\nyou lose...")
-        print("wins: {0}    draws: {1}    losses: {2}".format(wins, draws, losses))
-        time.sleep(2)
-        continue
-
-    if moveDict.get(playerMove) == 1 and pcMove == 3: # correction for paper beats rock
-        pcMove = -3
-
-    if moveDict.get(playerMove, 0) == 0:
+    playerMove = input(f"\n{blue}please enter your move{clear}: ").lower()        # asks player for move
+    
+    if moveDict.get(playerMove) == None:
         if errorCount == 2:
-            print("\ncome on...")
-            time.sleep(1)
+            print(f"\n{red}come on...{clear}")
             errorCount = 0
             continue
-        print("\nplease enter a valid answer")
+
+        print(f"\n{red}please enter a valid answer{clear}")
         errorCount += 1
         continue
 
-    time.sleep(0.6)
+    gamesPlayed += 1
 
-    print("\nthe computer chose {0}...".format(pcMoveDict.get(pcMove)))
+    if pcMove == 1 and moveDict[playerMove] == 3: # small workaround for paper beats rock
+        playerMove = "correction"
 
-    time.sleep(0.3)
+    elif moveDict[playerMove] == 1 and pcMove == 3: # correction for paper beats rock
+        pcMove = -3
+
+    print(f"\nthe computer chose {blue}{pcMoveDict[pcMove]}{clear}...")
 
     # determines outcome of game:
-    if moveDict.get(playerMove) == pcMove:
-        print("\nit's a draw!")
+    if moveDict[playerMove] == pcMove:
+        print(f"\n{yellow}it's a draw!")
         draws += 1
 
-    elif moveDict.get(playerMove) > pcMove:
-        print("\nyou win!")
+    elif moveDict[playerMove] > pcMove:
+        print(f"\n{green}you win!")
         wins += 1
 
     else:
-        print("\nyou lose...")
+        print(f"\n{red}you lose...")
         losses += 1
 
-    print("wins: {0}    draws: {1}    losses: {2}".format(wins, draws, losses))
-    time.sleep(2)
+    winPercentage = round((wins / gamesPlayed) * 100, 2)
+
+    print(f"{green}wins{clear}: {wins}    {yellow}draws{clear}: {draws}    {red}losses{clear}: {losses}")
+    print(f"{green}percentage won{clear}: {winPercentage}%\n--------------------------------------------")
